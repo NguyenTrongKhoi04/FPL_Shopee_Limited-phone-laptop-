7381df02c185d958ef097e8fb94d2149644de274
NguyenTrongKhoi04 <nguyentrongkhoi17062004@gmail.com>
2024-05-07 20:03:48 +0700

    test flash URL

--- test.php
+++ test.php
@@ -1,14 +1,14 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    echo"<pre>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<pre>";
     print_r($_POST);
     echo "</pre>";
 
     $arr = [];
-    foreach($_POST as $key => $value){
+    foreach ($_POST as $key => $value) {
         $arr[] = [$value];
     }
-    echo"<pre>";
+    echo "<pre>";
     print_r($arr);
     echo "</pre>";
     die;
@@ -25,49 +25,49 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
 <body>
     <form method="POST">
-    <table border="1">
-        <thead>
-            <tr>
-                <th>ID Pro</th>
-                <th>ID Detail</th>
-                <th>Name</th>
-                <th>Giá</th>
-                <th>size</th>
-                <th>số lượng</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    1
-                    <input type="hidden" value="2" name="idpro[]">
-                </td>
-                <td>
-                    12
-                    <input type="hidden" value="12" name="iddetail[]">
-                </td>
-                <td>Name</td>
-                <td>Giá</td>
-                <td>size</td>
-                <td><input type="number" name="count[]"></td>
-            </tr>
-            <tr>
-                <td>
-                    2
-                    <input type="hidden" value="2" name="idpro[]">
-                </td>
-                <td>
-                    13
-                    <input type="hidden" value="13" name="iddetail[]">
-                </td>
-                <td>Name</td>
-                <td>Giá</td>
-                <td>size</td>
-                <td><input type="number" name="count[]"></td>
-            </tr>
-        </tbody>
-    </table>
-    <button type="submit">Gửi</button>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ID Pro</th>
+                    <th>ID Detail</th>
+                    <th>Name</th>
+                    <th>Giá</th>
+                    <th>size</th>
+                    <th>số lượng</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        1
+                        <input type="hidden" value="2" name="idpro[]">
+                    </td>
+                    <td>
+                        12
+                        <input type="hidden" value="12" name="iddetail[]">
+                    </td>
+                    <td>Name</td>
+                    <td>Giá</td>
+                    <td>size</td>
+                    <td><input type="number" name="count[]"></td>
+                </tr>
+                <tr>
+                    <td>
+                        2
+                        <input type="hidden" value="2" name="idpro[]">
+                    </td>
+                    <td>
+                        13
+                        <input type="hidden" value="13" name="iddetail[]">
+                    </td>
+                    <td>Name</td>
+                    <td>Giá</td>
+                    <td>size</td>
+                    <td><input type="number" name="count[]"></td>
+                </tr>
+            </tbody>
+        </table>
+        <button type="submit">Gửi</button>
     </form>
 </body>
 
